c455027bfac90397804fd33f7197b7545f32de85
Sriraj Bura <sriraj.bura16@gmail.com>
2023-07-19 12:27:35 -0400

    Change CLI name and description

--- cmd/root.go
+++ cmd/root.go
@@ -14,14 +14,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "chromium-shortcuts",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "mini",
+	Short: "Add url shortcuts for chromium browsers in your terminal",
+	Long: ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
